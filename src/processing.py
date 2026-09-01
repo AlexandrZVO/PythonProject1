f94947382d926b8192e6972ec03010d6ee6f03c9
Alexandr <Sasha300678@bk.ru>
2026-09-01 08:39:31 +0300

    init def filter_by_state, def sort_by_date

---
+++ src/processing.py
@@ -0,0 +1,40 @@
+def filter_by_state(store: list, state: str = "EXECUTED") -> list:
+    """Фильтрует список словарей, оставляя только те, у которых ключ 'state' равен значению `state`."""
+    result = []
+    for item in store:
+        if item.get("state") == state:
+            result.append(item)
+    return result
+
+
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+
+
+from typing import Dict, List
+
+
+def sort_by_date(data: List[Dict]) -> List[Dict]:
+    """функцию, которая принимает список словарей и возвращать новый список, отсортированный по дате"""
+    sorted_data = sorted(data, key=lambda x: x["date"])
+    return sorted_data
+
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
